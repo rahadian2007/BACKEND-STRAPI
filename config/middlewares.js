de0ec12ccbd0004ae0df7b6cffd9e81560f5378c
rahadian2007 <maxrahadian@gmail.com>
2025-06-16 19:41:56 +0700

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,7 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['http://localhost:3000'],
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
